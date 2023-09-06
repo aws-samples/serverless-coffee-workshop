4f28b0642ecaf9e7ff0c52945f71d6faeb6cc5a0
Khairul Habib <mkhabib@amazon.id>
2023-09-06 10:55:55 +0700

    change url and endpoint to own backend

--- frontends/vue-barista-app/src/main.js
+++ frontends/vue-barista-app/src/main.js
@@ -30,10 +30,10 @@ import 'vue3-tel-input/dist/vue3-tel-input.css'
 
 Amplify.configure({
   Auth: {
-    region: '<< ENTER YOUR VALUE >>',
-    identityPoolRegion: '<< ENTER YOUR VALUE >>',
-    userPoolId: '<< ENTER YOUR VALUE >>',
-    userPoolWebClientId: '<< ENTER YOUR VALUE >>',
+    region: 'ap-southeast-1',
+    identityPoolRegion: 'ap-southeast-1',
+    userPoolId: 'ap-southeast-1_WpLE8AlIr',
+    userPoolWebClientId: '446dboc3mqtkarht692s56e3oi',
     mandatorySignIn: false,
     authenticationFlowType: 'CUSTOM_AUTH',
   }
@@ -47,15 +47,15 @@ app.config.globalProperties.$appName = 'Barista'
 app.config.globalProperties.$adminApp = true
 
 // ** Backend config **
-app.config.globalProperties.$region = '<< ENTER YOUR VALUE >>'
-app.config.globalProperties.$APIurl = '<< ENTER YOUR VALUE >>'
-app.config.globalProperties.$APIconfigURL = '<< ENTER YOUR VALUE >>'
-app.config.globalProperties.$ConfigEndpoint = '<< ENTER YOUR VALUE >>'
+app.config.globalProperties.$region = 'ap-southeast-1'
+app.config.globalProperties.$APIurl = 'https://en0zvine6g.execute-api.ap-southeast-1.amazonaws.com/Prod'
+app.config.globalProperties.$APIconfigURL = 'https://4rkb07gzf1.execute-api.ap-southeast-1.amazonaws.com/Prod/'
+app.config.globalProperties.$ConfigEndpoint = 'https://4rkb07gzf1.execute-api.ap-southeast-1.amazonaws.com/Prod/config'
 
 // ** Websocket connection **
 //  PoolId: Retrieve this with the CLI command: aws cognito-identity list-identity-pools --max-results 10 --region <<REGION>>
-app.config.globalProperties.$poolId = '<< ENTER YOUR VALUE >>' // 'YourCognitoIdentityPoolId'
+app.config.globalProperties.$poolId = 'ap-southeast-1:01c4c338-5d62-4be8-9a6c-7e750390ff97' // 'YourCognitoIdentityPoolId'
 //  IoTendpoint: Retrieve this with the CLI command: aws iot describe-endpoint --endpoint-type iot:Data-ATS --region us-west-2
-app.config.globalProperties.$host = '<< ENTER YOUR VALUE >>' // 'YourAwsIoTEndpoint', e.g. 'prefix.iot.us-east-1.amazonaws.com'
+app.config.globalProperties.$host = 'a3tw82l7ucghei-ats.iot.ap-southeast-1.amazonaws.com' // 'YourAwsIoTEndpoint', e.g. 'prefix.iot.us-east-1.amazonaws.com'
 
 app.mount('#app')
